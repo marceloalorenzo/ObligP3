c4ece946740bf112ac2e943d45ca5d64242f8b40
mpaniagua15 <mpaniagua1535@gmail.com>
2018-07-12 19:27:32 -0300

    Cree modulo CursoCocina.cpp

---
+++ CursoCocina.cpp
@@ -0,0 +1,36 @@
+#include "CursoCocina.h"
+
+/** 1. Registrar los nombres de todas las asignaturas que componen la carrera.
+ Al momento de registrar cada una, se le asignará también un número que la
+ identificará posteriormente en el programa. La primera asignatura en ser registrada
+ tendrá el número 0, la siguiente el número 1, la siguiente el número 2, y así sucesivamente. */
+
+
+/** 2. Listar número y nombre de todas las asignaturas registradas en el sistema,
+ ordenadas por número de asignatura de menor a mayor. */
+
+
+/** 3. Dados los números que identifican a dos asignaturas, agregar una previatura entre ellas,
+ cuidando que no se forme un ciclo de previaturas en el sistema. */
+
+
+/** 4. Dado el número que identifica a una asignatura, obtener un listado conteniendo
+ número y nombre de todas sus previas (no solamente de las previas inmediatas,
+ sino de todas sus asignaturas previas). */
+
+
+/** 5. Ingresar un nuevo alumno a la academia, chequeando que no existiera previamente. */
+void ingresarNuevoAlumno(Alumnos &a, Alumno alumno){
+}
+
+/** 6. Listar los datos básicos de todos los alumnos de la academia,
+ ordenados por cédula de menor a mayor. */
+
+
+/** 7. Agregar una nueva aprobación a la escolaridad de un alumno,
+ verificando que el alumno esté registrado en el sistema, que no tenga la asignatura
+ aprobada de antes y también que tenga salvadas sus previas inmediatas. */
+
+
+/** 8. Dada la cédula de un alumno, listar su escolaridad, ordenada cronológicamente
+ por fecha de aprobación. Se debe verificar que el alumno esté registrado en el sistema. */
