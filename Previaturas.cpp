510f5df3880e15ca426d2b6fd62eae0251aa0b55
FedeMontero <37647255+FedeMontero@users.noreply.github.com>
2018-07-14 23:02:23 -0300

    Update Previaturas.cpp

--- Previaturas.cpp
+++ Previaturas.cpp
@@ -6,19 +6,21 @@ l=NULL;
 
 void insFront(ListaAdy &l, int n){
   {
-Lista aux = new Nodo;
-aux -> info = n;
-aux -> sig = l;
+ListaAdy aux;
+aux=new nodo;
+aux->numAsignatura=n;
+aux->sig=l;
 l = aux;
 }
 
 Boolean pertenece(ListaAdy l, int n){
-boolean existe=FALSE;
-while(l!=NULL)
-  if(l->info==n)
-    existe= TRUE;
-  else
-    l=l->sig;
+boolean esta = FALSE;
+while ((!esta) && (l != NULL))
+if (L->numAsignatura == n)
+esta = TRUE;
+else
+l = l->sig;
+return esta;
 }
   return existe;
 }
@@ -37,15 +39,24 @@ int largo(ListaAdy l){
 
 void desplegarListaAdy(ListaAdy l){
 while(l!=NULL)
-  printf("%d",l->info);
+  printf("%d",l->numAsignatura);
   l=l->sig;
+}
 }
 /** Crear un grafo de N vértices y sin aristas */
 void crearGrafoPreviaturas(Previaturas &p){
+for (int i=0; i<N; i++)
+Crear(p[i]);
+}
 
 /** Dados dos números de asignaturas agregar una previatura entre ellas */
 void agregarPreviatura(Previaturas &p, int a1, int a2){
-
+InsFront(p[a1],a2);
+if (a1 != a2)
+InsFront(p[a2],a1);
+  
 /** Dado el número que identifica a una asignatura,
  obtener un listado conteniendo número y nombre de todas sus previas */
-void listarPrevias(Previaturas p, int asignatura){
+void listarPrevias(Previaturas p, int numAsignatura){
+  desplegarListaAdy(p[asignatura]);
+}
