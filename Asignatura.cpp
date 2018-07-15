f4edc7fe883b8b93cfed7f4fe28347b413649acb
FedeMontero <37647255+FedeMontero@users.noreply.github.com>
2018-07-14 21:24:09 -0300

    Update Asignatura.cpp

--- Asignatura.cpp
+++ Asignatura.cpp
@@ -1,1 +1,15 @@
 #include "Asignatura.h"
+void cargarAsignatura(Asignatura &a, int num){
+
+/** Selectora del nombre */
+void darNombre(String &s, Asignatura a){
+  strcop(s,a.nombre);
+}
+
+/** Selectora del numero */
+int darNumero(Asignatura a){
+  return (a.numero);
+}
+
+/** Muestra la asignatura en pantalla */
+void printAsignatura(Asignatura a){
