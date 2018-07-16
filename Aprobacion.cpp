ff51ac7ed163f88dd21dd143afb0ab102173a593
marceloalorenzo <32404538+marceloalorenzo@users.noreply.github.com>
2018-07-15 21:02:45 -0300

    aprobaciones, aun falta

--- Aprobacion.cpp
+++ Aprobacion.cpp
@@ -1,15 +1,26 @@
 #include "Aprobacion.h"
 
 /** Carga una Aprobacion recibiendo los datos desde teclado */
-void cargarAprobacion(Aprobacion &a){
+void cargarAprobacion(Aprobacion &a)
+{
+    crearAsignaturas(a.asignatura);
+    crearFecha(a.fechaAprobacion);
+    printf("Ingrese calificación: ");
+    scanf("&d",a.calificacion);
 }
 
 /** Muestra por pantalla los datos de la Aprobacion */
-void printAprobacion(Aprobacion a){
+void printAprobacion(Aprobacion a)
+{
+    printAsignatura(a.asignatura);
+    printFecha(a.fechaAprobacion);
+    printf("Calificación %d",a.calificacion);
 }
 
 /** Selectora del numero de Asignatura */
-int darNumeroAsig(Aprobacion a){
+int darNumeroAsig(Aprobacion a)
+{
+    strCop(s,a.asignatura);
 }
 
 /** Selectora del nombre de Asignatura */
